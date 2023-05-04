5c1a59670973111516dfdae6bd91940f26b4f9a3
Newball <newball875@gmail.com>
2023-05-04 11:43:11 +0200

    Update ArianeGame.java

--- core/src/net/ariane/ArianeGame.java
+++ core/src/net/ariane/ArianeGame.java
@@ -2,6 +2,7 @@ package net.ariane;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 public class ArianeGame extends ApplicationAdapter {
 	ShapeRenderer shape;
 	Texture img;
+	int i=0;
 	
 	@Override
 	public void create () {
@@ -18,6 +20,11 @@ public class ArianeGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.graphics.setWindowedMode(800, 800);
+		if(i==0){
+			System.out.println(Gdx.graphics.getWidth());
+			i=i+1;
+		}
 		//shape.begin(ShapeRenderer.ShapeType.Filled);
 	}
 	
