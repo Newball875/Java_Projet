628603e6f916ad20fd0f20a6a20e322b9e602533
capitaine robin <rcapitai@poireauxl.kilimandjaro.calais.fr>
2023-05-04 11:17:32 +0200

    Directory

---
+++ core/src/net/ariane/Entites/Joueur.java
@@ -0,0 +1,11 @@
+package net.ariane.Entites;
+
+public class Joueur extends Entite{
+
+    public static final int MAXLIFE = 100;
+    public static final int SPEED = 5;
+
+    public Joueur(){
+        super();
+    }
+}
