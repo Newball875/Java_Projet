53c3f2b9af1c1c8c234fe02250f5054c36cc68c1
capitaine robin <rcapitai@poireauxl.kilimandjaro.calais.fr>
2023-05-04 11:43:03 +0200

    Directory

---
+++ core/src/net/ariane/Entites/Ennemi.java
@@ -0,0 +1,14 @@
+package net.ariane.Entites;
+
+public class Ennemi extends Entite{
+
+    public static final int MAXLIFE = 20;
+    public static final int SPEED = 1;
+    public static final int SIZEX = 20;
+    public static final int SIZEY = 20;
+
+    public Ennemi(){
+        super();
+    }
+    
+}
