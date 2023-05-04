d5b4ebcc0f9835be68a8491e806a139185d17cf8
Newball <newball875@gmail.com>
2023-05-04 11:25:39 +0200

    Oui

--- core/src/net/ariane/ArianeGame.java
+++ core/src/net/ariane/ArianeGame.java
@@ -1,31 +1,24 @@
 package net.ariane;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class ArianeGame extends ApplicationAdapter {
-	SpriteBatch batch;
+	ShapeRenderer shape;
 	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shape=new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//shape.begin(ShapeRenderer.ShapeType.Filled);
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
