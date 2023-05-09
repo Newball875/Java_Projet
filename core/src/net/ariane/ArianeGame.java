eb66ba733251e3ab436a084cfe396f71b3705164
Newball <newball875@gmail.com>
2023-05-09 10:56:18 +0200

    Taille fenêtre

--- core/src/net/ariane/ArianeGame.java
+++ core/src/net/ariane/ArianeGame.java
@@ -21,8 +21,6 @@ public class ArianeGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		Gdx.graphics.setTitle("Zaq");
-		this.resize(800, 800);
 		shape.begin(ShapeRenderer.ShapeType.Filled);
 		if(i==0){
 			System.out.println(Gdx.graphics.getWidth());
