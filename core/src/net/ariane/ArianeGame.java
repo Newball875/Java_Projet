14a40eae103e94e523dd6d9e7aedc285ffae893f
capitaine robin <rcapitai@poireauxl.kilimandjaro.calais.fr>
2023-05-09 11:30:03 +0200

    changement

--- core/src/net/ariane/ArianeGame.java
+++ core/src/net/ariane/ArianeGame.java
@@ -1,17 +1,20 @@
 package net.ariane;
 
+import net.ariane.bullet.Bullet;
 import net.ariane.mobs.*;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import net.ariane.mobs.ennemis.Classique;
 
 import java.util.HashSet;
 
 public class ArianeGame extends ApplicationAdapter {
 	Joueur zac;
 	HashSet<Ennemi>ennemis=new HashSet<Ennemi>();
+	HashSet<Bullet>balles=new HashSet<Bullet>();
 	ShapeRenderer shape;
 	int i=0;
 	
@@ -19,12 +22,21 @@ public class ArianeGame extends ApplicationAdapter {
 	public void create () {
 		shape=new ShapeRenderer();
 		zac=new Joueur();
-		createEnnemi();
+		createEnnemi(100,100);
 	}
 
-	public void createEnnemi () {
-		Ennemi ennemi=new Ennemi(100,100);
+	public void createEnnemi (int X, int Y) {
+		Ennemi ennemi=new Classique(X,Y);
 		ennemis.add(ennemi);
+	}
+
+	public void niveau1() throws InterruptedException {
+		shape=new ShapeRenderer();
+		zac=new Joueur();
+		for(int i=0;i<10;i++) {
+			createEnnemi(i*10+5,100);
+			java.util.concurrent.TimeUnit.SECONDS.sleep(2);
+		}
 	}
 
 	@Override
--- core/src/net/ariane/mobs/ennemis/Navar.java
+++ core/src/net/ariane/mobs/ennemis/Navar.java
@@ -3,10 +3,12 @@ package net.ariane.mobs.ennemis;
 import net.ariane.mobs.Ennemi;
 import net.ariane.weapon.Destroyer;
 
-public class Classique extends Ennemi {
-    public Classique(int X, int Y){
+public class Navar extends Ennemi{
+
+    public Navar(int X, int Y){
         super(X,Y);
         this.setLife(20);
         this.setWeapon(new Destroyer());
     }
+
 }
