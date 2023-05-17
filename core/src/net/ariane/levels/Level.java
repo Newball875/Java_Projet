3395ede076a9feb11b145087055cfff9ade7c23e
Newball <newball875@gmail.com>
2023-05-17 11:46:19 +0200

    Update Level.java

--- core/src/net/ariane/levels/Level.java
+++ core/src/net/ariane/levels/Level.java
@@ -1,19 +1,26 @@
 package net.ariane.levels;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 import net.ariane.mobs.Ennemi;
 
 public class Level {
-	private HashSet<Ennemi>ennemis=new HashSet<Ennemi>();
-	protected HashSet<Ennemi>[]vagues;
+	public HashSet<Ennemi>ennemis=new HashSet<Ennemi>();
+	protected Ennemi[][] vagues;
 	private int nombre;		//Nombre de vagues
 	private int actuel=0;	//Vague actuelle
 
 	public Level(int nombre){
 		setNombre(nombre);
+		this.vagues=new Ennemi[nombre][];
 	}
 
 	public void setNombre(int nombre){this.nombre=nombre;}
 	public int getNombre(){return this.nombre;}
+
+	public int getNombreEnnemis(){
+		ArrayList<Ennemi> mechant=new ArrayList<>(this.ennemis);
+		return mechant.size();
+	}
 }
